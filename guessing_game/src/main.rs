dc1c08e5b6fc9d563d231fdfa7c45db2d9455311
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-10 18:22:47 -0400

    add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Guess a number!");
+    println!("Input a number:");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line. ")
+    println!("Guess a number!");
+
+}
