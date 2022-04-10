beb6b5ed407da3ecf845d49aa174e31a65a84aa3
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-10 19:36:51 -0400

    converted str to int in guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,19 @@
 use std::io;
+// use rand::prelude::*;
+use rand::Rng;
 
 fn main() {
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("random number is {}", random_num);
     println!("Guess a number!");
     println!("Input a number:");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line. ")
-    println!("Guess a number!");
-
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line. ");
+    println!("You guessed {}", guess);
+    
+    if random_num == guess.trim().parse().unwrap() {
+        println!("you guess right!")
+    }
 }
