4c4707ad75e1dca4a5f5373bdef520ecfc98de3a
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-10 20:16:22 -0400

    add types and match less and greater

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
-// use rand::prelude::*;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let random_num = rand::thread_rng().gen_range(1..101);
+    let random_num: u32 = rand::thread_rng().gen_range(1..101);
     println!("random number is {}", random_num);
     println!("Guess a number!");
     println!("Input a number:");
@@ -12,8 +12,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line. ");
     println!("You guessed {}", guess);
-    
-    if random_num == guess.trim().parse().unwrap() {
-        println!("you guess right!")
+    let guess_int: u32 = guess.trim().parse().expect("Error, 🚩 Please input a number! 🚩");
+    match guess_int.cmp(&random_num) {
+        Ordering::Less => println!("too small 🍓"),
+        Ordering::Greater => println!("too large 🍌"),
+        Ordering::Equal => println!("nice 6️⃣ 9️⃣"),
     }
+    // if random_num == guess.trim().parse().unwrap() {
+    //     println!("you guess right!")
+    // }
 }
