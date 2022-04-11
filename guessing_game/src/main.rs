e9c624febae4e231df9edeb2c66bd019f81469fb
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-10 20:29:42 -0400

    attempt process exit code depending on user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,32 @@
 use std::io;
+use std::process;
 use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    let random_num: u32 = rand::thread_rng().gen_range(1..101);
-    println!("random number is {}", random_num);
-    println!("Guess a number!");
-    println!("Input a number:");
-    let mut guess = String::new();
+    println!("This is a guessing game.");
+    println!("Are you ready?");
+    let mut ready = String::new();
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line. ");
-    println!("You guessed {}", guess);
-    let guess_int: u32 = guess.trim().parse().expect("Error, 🚩 Please input a number! 🚩");
-    match guess_int.cmp(&random_num) {
-        Ordering::Less => println!("too small 🍓"),
-        Ordering::Greater => println!("too large 🍌"),
-        Ordering::Equal => println!("nice 6️⃣ 9️⃣"),
+        .read_line(&mut ready)
+        .expect("Yes or No");
+    if ready == "n" {
+        process::exit(0x0100);
     }
-    // if random_num == guess.trim().parse().unwrap() {
-    //     println!("you guess right!")
-    // }
+    let random_num: u32 = rand::thread_rng().gen_range(1..101);
+    println!("Input a number:");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line. ");
+            println!("You guessed {}", guess);
+            let guess_int: u32 = guess.trim().parse().expect("Error, 🚩 Please input a number! 🚩");
+            match guess_int.cmp(&random_num) {
+                Ordering::Less => println!("too small 🍓"),
+                Ordering::Greater => println!("too large 🍌"),
+                Ordering::Equal => {println!("nice 6️⃣ 9️⃣ You win! 🌟"); break;},
+        }
+    }
+ 
 }
